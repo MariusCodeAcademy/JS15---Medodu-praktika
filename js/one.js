362d52830620c9521a08e88f3477871e9ec05091
Marius <marius@example.com>
2022-03-14 08:45:02 +0200

    students

--- js/one.js
+++ js/one.js
@@ -1,7 +1,34 @@
 console.log('one');
 
 const numbers = [1, 2, 3, 5.3, 8, -8, -5.5, -6, -16, -5, -9, 4.2, 5, 12, -6, 45];
-
+console.log('numbers ===', numbers);
 //  3. Sukurkite ir atvaizduokite masyvą su dvigubomis reikšmėmis;
-
+const doubleNumbers = numbers.map((sk) => {
+  const dobleValue = sk * 2;
+  return dobleValue;
+});
+console.log('doubleNumbers ===', doubleNumbers);
 //  4. Sukurkite ir atvaizduokite masyvą su kvadratu pekltomis reikšmėmis;
+
+//  5. Sukurkite ir atvaizduokite masyvą su reikšmėmis padaugintomis iš jų vietos masyve indekso
+
+//  6. Atrinkti tiktai teigimų elementų masyvą
+
+//  7. Atrinkti vertes didesnes uz 3
+//  7.1 Parasyti funkcija kuri ima 2 argumentus, masyva ir skaiciu uz kuri didesnes vertes grazina ir atspausdina.
+
+// 12. Suapvalinti visas masyvo reikšmes iki sveikų skaičių
+
+// 13. Atrinkti kas antrą elementą į naują masyvą
+
+// 15. Atspausdinti kiekvieną masyvo reikšmę atskiroje eilutėje: [0] => 64.
+// 15.1 Grazinti nauja masyva kuriame yra objektai
+// [
+//   {index: 0, reiksme: 64}
+//   {index: 1, reiksme: 25}
+//    ...
+// ]
+
+// 16. Suskaičiuoti visų elementų sumą
+
+// 18. Rasti didžiausią skaičių masyve
---
+++ js/students.js
@@ -0,0 +1,53 @@
+const students = [
+  {
+    name: 'James',
+    gender: 'male',
+    age: 25,
+    town: 'Vilnius',
+    hasCar: true,
+  },
+  {
+    name: 'Jill',
+    gender: 'female',
+    age: 28,
+    town: 'Kaunas',
+    hasCar: true,
+  },
+  {
+    name: 'Mike',
+    gender: 'male',
+    age: 18,
+    town: 'Kaunas',
+    hasCar: false,
+  },
+  {
+    name: 'Jane',
+    gender: 'female',
+    age: 22,
+    town: 'Klaipeda',
+    hasCar: false,
+  },
+  {
+    name: 'Jannet',
+    gender: 'female',
+    age: 29,
+    town: 'Klaipeda',
+    hasCar: true,
+  },
+  {
+    name: 'Cory',
+    gender: 'male',
+    age: 30,
+    town: 'Siauliai',
+    hasCar: true,
+  },
+  {
+    name: 'Minks',
+    gender: 'male',
+    age: 19,
+    town: 'Siauliai',
+    hasCar: false,
+  },
+];
+
+// st1. sugeneruoti studentu nerikiuota sarasa htmle kuriame butu studentu vardai
