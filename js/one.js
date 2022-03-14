102a97efe3c9cb7ba5b0fe70ac5220033dbaaaef
Marius <marius@example.com>
2022-03-14 08:22:38 +0200

    praktice init

---
+++ js/one.js
@@ -0,0 +1,7 @@
+console.log('one');
+
+const numbers = [1, 2, 3, 5.3, 8, -8, -5.5, -6, -16, -5, -9, 4.2, 5, 12, -6, 45];
+
+//  3. Sukurkite ir atvaizduokite masyvą su dvigubomis reikšmėmis;
+
+//  4. Sukurkite ir atvaizduokite masyvą su kvadratu pekltomis reikšmėmis;
